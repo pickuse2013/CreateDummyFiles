e0827bb61e219a54b025df22389c5faa98f58298
pickuse <pickuse2013@gmail.com>
2021-06-17 20:42:25 +0800

    update

---
+++ CreateDummyFiles.ps1
@@ -0,0 +1,41 @@
+﻿param(
+$amount = $(throw "Please give an amount of files to be created")
+, $size = $(throw "Please give a the size of the files")
+, $folder = $(throw "Please give an output folder wehere the files need to be created")
+, $name = $null
+, $extension = $null
+)
+CLS
+# Check for input
+if(Test-Path $folder)
+{
+if($name -eq $null)
+{
+Write-Host "No filename given. Using default setting 'dummy'" -ForegroundColor Yellow
+$name = 'dummy'
+}
+ 
+if($extension -eq $null)
+{
+Write-Host "No filename extension given. Using default setting '.txt'" -ForegroundColor Yellow
+$extension = 'txt'
+}
+elseif($extension -contains '.')
+{
+$extension = $extension.Substring(($extension.LastIndexOf(".") + 1), ($extension.Length - 1))
+}
+ 
+for($i = 1; $i -le $amount; $i++)
+{
+$path = $folder + '\' + $name + '_' + $i + '.' + $extension
+$file = [io.file]::Create($path)
+$file.SetLength($size)
+$file.Close
+sleep 0.5
+}
+ 
+}
+else{
+Write-Host "The folder $folder doesn't exist" -ForegroundColor Red
+Exit(0)
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# CreateDummyFiles_PowerShell
+# Create Dummy Files with PowerShell
+
+run:
+```shell
+.\CreateDummyFiles.ps1 <amount of files> <files sizes> <output folder>
+```
+
+run 10 of 1KB file:
+```shell
+.\CreateDummyFiles.ps1 10 1024 D:\testFolder
+```
+
+
+[Source](https://www.sqlstad.nl/powershell/create-dummy-files-powershell/)
