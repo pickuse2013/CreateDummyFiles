a92f765ce641ded305e75c101163b96056ab496a
pickuse <pickuse2013@gmail.com>
2021-06-28 15:55:05 +0800

    code update

--- create100kDummyFiles.py
+++ create100kDummyFiles.py
@@ -13,8 +13,5 @@ work_path = os.path.abspath(os.path.dirname(__file__))
 if not os.path.exists(test_path):
     os.mkdir(test_path)
 
-# switch folder
-os.chdir(os.path.expanduser(work_path + "/" + test_path))
-
 for i in range(n):
-    subprocess.call(['touch', "testFile"+str(i).zfill(len(str(n)))+".test"])
+    open(work_path + "\\" + test_path + "\\" + "testFile"+str(i).zfill(len(str(n)))+".test", 'a').close()
--- create100kDummyPaths.py
+++ create100kDummyPaths.py
@@ -2,20 +2,20 @@
 import os
 import subprocess
 
+# test folder (auto create)
 test_path = "test_folder2"
 
-#put the number as you wish
-n = 100
+# put the number as you wish
+n = 100000
 
 work_path = os.path.abspath(os.path.dirname(__file__))
 
+if not os.path.exists(test_path):
+    os.mkdir(test_path)
+
 for i in range(n):
     # create folder
-    path_name = test_path + str(i).zfill(len(str(n)))
+    path_name = test_path + "\\test" + str(i).zfill(len(str(n)))
     if not os.path.exists(path_name):
         os.mkdir(path_name)
-
-    # switch folder
-    os.chdir(os.path.expanduser(work_path + "/" + path_name))
-    
-    subprocess.call(['touch', "testFile0001.test"])
+    open(work_path + "\\" + path_name + "\\" + "testFile001.test", 'a').close()
